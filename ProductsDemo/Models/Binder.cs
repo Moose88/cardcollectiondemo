5c18177a0010ddbc6fa26b231426773abdfea800
mbour <mbourgoine21@gmail.com>
2026-03-12 13:17:55 -0700

    Added a Binder model for new binder feature.

---
+++ ProductsDemo/Models/Binder.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductsDemo.Models
+{
+    public class Binder
+    {
+        public int Id { get; set; }
+        public string GameName { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
